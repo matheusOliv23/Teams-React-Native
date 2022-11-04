d452721cac69f07f66d7ea3f37105a9d23dc8552
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-04 20:20:24 -0300

    feat: add highlight component

---
+++ src/components/Highlight/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  margin: 32px;
+`;
+
+export const Title = styled.Text`
+  text-align: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD}px;
+  color: ${({ theme }) => theme.COLORS.WHITE}px;
+`;
+
+export const Subtitle = styled.Text`
+  text-align: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR}px;
+  color: ${({ theme }) => theme.COLORS.GRAY_300}px;
+`;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 import * as S from "./styles";
 
 export default function Groups() {
   return (
     <S.Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com sua turma" />
     </S.Container>
   );
 }
