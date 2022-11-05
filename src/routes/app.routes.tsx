74da33dfaedb5f5c8996c7ecc0db952f1380228f
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-05 17:49:12 -0300

    chore: add async storage

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,15 +2,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Groups from "@screens/Groups";
 import NewGroup from "@screens/NewGroup";
 import Players from "@screens/Players";
+import { View } from "react-native";
+import { useTheme } from "styled-components/native";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes() {
+  const { COLORS } = useTheme();
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="groups" component={Groups} />
-      <Screen name="players" component={Players} />
-      <Screen name="new" component={NewGroup} />
-    </Navigator>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="groups" component={Groups} />
+        <Screen name="players" component={Players} />
+        <Screen name="new" component={NewGroup} />
+      </Navigator>
+    </View>
   );
 }
