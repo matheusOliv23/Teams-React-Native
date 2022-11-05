6c3a4a407023fb76ee8d08d3498b5d360e7be360
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-05 11:51:08 -0300

    feat: add safe area view to screens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,20 @@
 import * as S from "./styles";
 import logo from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
   showBackButton?: boolean;
 };
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
   return (
     <S.Container>
       {showBackButton && (
-        <S.BackButton>
+        <S.BackButton onPress={handleGoBack}>
           <S.BackIcon />
         </S.BackButton>
       )}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,12 +4,17 @@ import { Button } from "@components/Button";
 import { ReactNode } from "react";
 import * as S from "./styles";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 interface NewGroupProps {
   children: ReactNode;
 }
 
 export default function NewGroup() {
+  const navigation = useNavigation();
+  function handleNew() {
+    navigation.navigate("players", { group: "Estudos" });
+  }
   return (
     <S.Container>
       <Header showBackButton />
@@ -20,7 +25,7 @@ export default function NewGroup() {
           subtitle="Crie a turma para add pessoas"
         />
         <Input placeholder="Nome da Turma" />
-        <Button style={{ marginTop: 20 }} title="Criar" />
+        <Button onPress={handleNew} style={{ marginTop: 20 }} title="Criar" />
       </S.Content>
     </S.Container>
   );
