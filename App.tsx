b52430eb168679fa828fdbb26399d359b5faef57
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-05 07:00:39 -0300

    feat: add screen Players interface

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import {
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
 import NewGroup from "@screens/NewGroup";
+import Players from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import * as S from "./styles";
+
+export default function Players() {
+  return (
+    <S.Container>
+      <Header showBackButton />
+      <Highlight
+        title="Nome da Turma"
+        subtitle="Adicione as pessoas e separe os times"
+      />
+    </S.Container>
+  );
+}
