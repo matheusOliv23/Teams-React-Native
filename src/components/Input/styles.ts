397b68d8a8039fcd15b9a60217a65586191bf427
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-05 06:53:16 -0300

    refactor: refactoring styled code with css helper

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,14 +1,17 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
+
 
 export const Container = styled(TextInput)`
-  flex: 1;
-  min-height: 56px;
-  max-height: 56px;
-  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-  color: ${({ theme }) => theme.COLORS.WHITE};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  border-radius: 6px;
-  padding: 16px;
+  ${({ theme }) => css`
+    flex: 1;
+    min-height: 56px;
+    max-height: 56px;
+    background-color: ${theme.COLORS.GRAY_700};
+    color: ${theme.COLORS.WHITE};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    border-radius: 6px;
+    padding: 16px;
+  `}
 `;
