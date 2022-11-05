168b1f9702219f86209e06c10c67955348022daa
matheusOliv23 <matheushenri1996@gmail.com>
2022-11-05 11:17:10 -0300

    feat: creating navigation structure

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import Groups from "@screens/Groups";
 import theme from "@theme/index";
 import { ThemeProvider } from "styled-components";
 import {
@@ -8,8 +7,7 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
-import NewGroup from "@screens/NewGroup";
-import Players from "@screens/Players";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
